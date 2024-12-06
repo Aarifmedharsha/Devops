2d4b0939b6e49612cee66700407523244c471401
Aarif  M <03aarif20@gmail.com>
2024-12-06 09:26:41 +0530

    Update main.tf

--- Test/main.tf
+++ Test/main.tf
@@ -2,8 +2,8 @@ provider "aws"{
   region = "us-west-2"
 }
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = "ami-04dd23e62ed049936"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
