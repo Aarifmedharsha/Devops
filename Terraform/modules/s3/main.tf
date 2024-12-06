025189daf4d3d4199e0146de28772202c155a2b7
Aarif  M <03aarif20@gmail.com>
2024-12-06 12:26:34 +0530

    Update main.tf

--- Terraform/modules/s3/main.tf
+++ Terraform/modules/s3/main.tf
@@ -1,15 +1,7 @@
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "secure-private-bucket-aarif"
-}
-
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.secure_bucket.id
   acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.secure_bucket.id
-  versioning_configuration {
-    status = "Enabled"
+  versioning {
+    enabled = true
   }
 }
